6c4b517d61b5c83e23cca6e83a13c6d4aee36b5d
MasudRana15924 <masudrana15924@outlook.com>
2024-03-28 23:53:21 +0600

    signature verification add

--- controller/webhook.controller.js
+++ controller/webhook.controller.js
@@ -1,7 +1,51 @@
+const https = require('https');
+const crypto = require('crypto');
 const WebhookModel = require("../models/webhook");
+
+const downloadCertificate = (certUrl) => {
+    return new Promise((resolve, reject) => {
+        https.get(certUrl, (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                resolve(data);
+            });
+        }).on('error', (err) => {
+            reject(err);
+        });
+    });
+};
+
+const verifySignature = async (message, signature, certUrl) => {
+    try {
+        // Download certificate
+        const cert = await downloadCertificate(certUrl);
+        console.log("cert",cert);
+        // Load certificate
+        const verifier = crypto.createVerify('sha256');
+        verifier.update(JSON.stringify(message));
+        // Verify signature
+        const isVerified = verifier.verify(cert, signature, 'base64');
+        return isVerified;
+    } catch (error) {
+        console.error('Error verifying signature:', error);
+        return false;
+    }
+};
 const createWebhook = async (req, res) => {
     const payload = req.body;
     console.log("Received payload:", payload);
+    // const signature = req.headers['x-amz-sns-message-id']; // Assuming signature is in header 'x-amz-sns-message-id'
+    // if (!signature) {
+    //     return res.status(400).send("Signature not found in headers");
+    // }
+    // Verify signature
+    // const isSignatureValid = verifySignature(payload, signature, payload.SigningCertURL);
+    // if (!isSignatureValid) {
+    //     return res.status(403).send("Signature verification failed");
+    // }
     switch (payload.Type) {
         case "SubscriptionConfirmation":
             await WebhookModel.createMessage(req.body);
